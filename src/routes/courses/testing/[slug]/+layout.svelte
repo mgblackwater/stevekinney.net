1433cf46bf9a9aa4f2323f751f2c11f23960b2e6
Steve Kinney <hello@stevekinney.net>
2024-09-29 12:28:50 -0600

    Add a link to the current file in development

--- src/routes/courses/testing/[slug]/+layout.svelte
+++ src/routes/courses/testing/[slug]/+layout.svelte
@@ -1,3 +1,19 @@
+<script lang="ts">
+	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
+	import projectRoot from 'virtual:project-root';
+</script>
+
+{#if dev}
+	<a
+		class="my-4 flex rounded-md border-2 border-primary-200 bg-primary-100 p-2 text-sm font-semibold shadow-sm transition-colors hover:border-primary-300 hover:bg-primary-200 dark:border-primary-900 dark:bg-primary-800 dark:text-primary-200"
+		href="vscode://file/{projectRoot}/content{$page.url.pathname}.md"
+	>
+		<span class="block">Open in Visual Studio Code.</span>
+		<span class="block text-primary-800">&rarr;</span>
+	</a>
+{/if}
+
 <header>
 	<h2>
 		<a href="/courses/testing" class=" text-primary-600 dark:text-primary-200">
