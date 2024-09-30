f576532d7bffa6081b5978a59b5ee7e12ecd3c1e
Steve Kinney <hello@stevekinney.net>
2024-09-30 07:42:59 -0600

    Open tabs in a new window

--- src/routes/courses/testing/[slug]/resources.svelte
+++ src/routes/courses/testing/[slug]/resources.svelte
@@ -4,6 +4,7 @@
 			<a
 				href="https://stackblitz.com/~/github.com/stevekinney/vitest-scratchpad?file=src/example.test.js"
 				class="block text-primary-600 decoration-primary-800 decoration-2 underline-offset-2 hover:underline dark:text-primary-400 dark:decoration-primary-600"
+				target="_blank"
 			>
 				Vitest Scratchpad
 			</a>
@@ -12,6 +13,7 @@
 			<a
 				href="https://github.com/stevekinney/introduction-to-testing"
 				class="block text-primary-600 decoration-primary-800 decoration-2 underline-offset-2 hover:underline dark:text-primary-400 dark:decoration-primary-600"
+				target="_blank"
 			>
 				Example Code Repository
 			</a>
